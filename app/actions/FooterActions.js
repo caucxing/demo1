302e07d57754fcb1d6c2bd01611757a3bfcd91e7
caucxing <154801313@qq.com>
2016-02-17 20:56:07 +0800

    tttt

---
+++ app/actions/FooterActions.js
@@ -0,0 +1,25 @@
+/**
+ * Created by caucxing on 2016-2-17.
+ */
+import alt from '../alt';
+
+class FooterActions {
+    constructor() {
+        this.generateActions(
+            'getTopCharactersSuccess',
+            'getTopCharactersFail'
+        );
+    }
+
+    getTopCharacters() {
+        $.ajax({ url: '/api/characters/top' })
+            .done((data) => {
+            this.actions.getTopCharactersSuccess(data)
+    })
+    .fail((jqXhr) => {
+            this.actions.getTopCharactersFail(jqXhr)
+    });
+    }
+}
+
+export default alt.createActions(FooterActions);
